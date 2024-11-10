9df92a491346c41095433a2c67258dc438ceb6ee
vishwa <78722643+vsgames42@users.noreply.github.com>
2024-11-10 12:33:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,13 @@ app.use(cors(
     {
         origin:["https://mern-tawny-xi.vercel.app/"],
         methods:["POST","GET","UPDATE","DELETE"],
-        credentials: true
+        credentials: true,
+        headers:{
+                    'Access-Control-Allow-Origin' : '*',
+                    "Access-Control-Allow-Credentials" : "true",
+                    "Access-Control-Allow-Methods" : "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+                    "Access-Control-Allow-Headers" : "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+                }
     }
 ))
 
