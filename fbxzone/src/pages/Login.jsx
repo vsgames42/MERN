491443ffd23b144331e31772a73113c6e90bffd0
vishwa <78722643+vsgames42@users.noreply.github.com>
2024-11-10 12:19:02 +0530

    Update Login.jsx

--- fbxzone/src/pages/Login.jsx
+++ fbxzone/src/pages/Login.jsx
@@ -17,8 +17,8 @@ function Login() {
                 baseURL:'https://server-steel-one.vercel.app/login',
                 headers:{
                     'Access-Control-Allow-Origin' : '*',
-                    "Access-Control-Allow-Credentials" : "true"
-                    "Access-Control-Allow-Methods" : "POST",
+                    "Access-Control-Allow-Credentials" : "true",
+                    "Access-Control-Allow-Methods" : "GET,OPTIONS,PATCH,DELETE,POST,PUT",
                     "Access-Control-Allow-Headers" : "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
                 },
             },
